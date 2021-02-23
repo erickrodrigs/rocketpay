64ff31af2f681c3d8ace114bcde919189b62efc2
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-02-23 17:30:06 -0300

    add unit tests

---
+++ test/rocketpay/numbers_test.exs
@@ -0,0 +1,17 @@
+defmodule Rocketpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Rocketpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "when there is a file with the given name, returns the sum of numbers" do
+      response = Numbers.sum_from_file("numbers")
+      assert response == {:ok, %{result: 37}}
+    end
+
+    test "when there is no file with the given name, returns an error" do
+      response = Numbers.sum_from_file("banana")
+      assert response == {:error, %{message: "Invalid file!"}}
+    end
+  end
+end
